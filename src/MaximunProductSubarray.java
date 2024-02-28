54b6bc4b80ac48045db69c0be6b269cc19017742
akki <akashpatole43@gmail.com>
2024-02-29 00:01:06 +0530

    testCommit

--- src/MaximunProductSubarray.java
+++ src/MaximunProductSubarray.java
@@ -71,7 +71,6 @@ class Solution01 {
 			if ((min * nums[i]) > prod) prod = (min * nums[i]);
 			
 			temp = (max * nums[i]) < nums[i] ? (max * nums[i]) : nums[i];
-//			if ((min * nums[i]) > temp) min = temp;
 			min = (min * nums[i]) > temp ? temp : (min * nums[i]);
 			
 			max = prod;
@@ -80,30 +79,27 @@ class Solution01 {
     	return product;
     }
 }
+ 
 
-class Solution02 {
-	public int maxProduct(int[] nums) {
-		int largestProduct = nums[0];
-        int minCurrentProduct = nums[0];
-        int maxCurrentProduct = nums[0];
-        
-        for (int i = 1; i < nums.length; i++) {
-        	int currentProduct = Math.max(maxCurrentProduct * nums[i], nums[i]);
-        	
-        	if (minCurrentProduct * nums[i] > currentProduct)
-        		currentProduct = minCurrentProduct * nums[i];
-        	
-        	int temp = Math.min(maxCurrentProduct * nums[i], nums[i]);
-        	
-        	if (minCurrentProduct * nums[i] > temp)
-        		minCurrentProduct = temp;
-        	else
-        		minCurrentProduct = minCurrentProduct * nums[i];
-        	
-        	maxCurrentProduct = currentProduct;
-        	largestProduct = Math.max(maxCurrentProduct, largestProduct);
-        }
-        
-		return largestProduct;
-	}
-}
+/* following is the commented code for reference 
+ * 
+ *
+ * class Solution02 { public int maxProduct(int[] nums) { int largestProduct =
+ * nums[0]; int minCurrentProduct = nums[0]; int maxCurrentProduct = nums[0];
+ * 
+ * for (int i = 1; i < nums.length; i++) { int currentProduct =
+ * Math.max(maxCurrentProduct * nums[i], nums[i]);
+ * 
+ * if (minCurrentProduct * nums[i] > currentProduct) currentProduct =
+ * minCurrentProduct * nums[i];
+ * 
+ * int temp = Math.min(maxCurrentProduct * nums[i], nums[i]);
+ * 
+ * if (minCurrentProduct * nums[i] > temp) minCurrentProduct = temp; else
+ * minCurrentProduct = minCurrentProduct * nums[i];
+ * 
+ * maxCurrentProduct = currentProduct; largestProduct =
+ * Math.max(maxCurrentProduct, largestProduct); }
+ * 
+ * return largestProduct; } }
+ */
