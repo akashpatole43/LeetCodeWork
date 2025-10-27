bf06d4dc652b0d9b839dcf06e1d02d21cec58bf6
akashpatole43 <61220660+akashpatole43@users.noreply.github.com>
2025-10-27 17:44:14 +0530

    MergeSortedArray

--- src/Easy/MergeTwoSortedList.java
+++ src/Easy/MergeTwoSortedList.java
@@ -1,3 +1,13 @@
+/*
+ * Description;
+ * 
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * 
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * 
+ * Return the head of the merged linked list.
+*/
+
 package Easy;
 
 // Merge two sorted linked list
