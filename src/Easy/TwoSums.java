3b9836ac742ef7b873a9b5c48b0326dd90642977
akashpatole43 <61220660+akashpatole43@users.noreply.github.com>
2025-10-31 14:36:30 +0530

    TwoSums

--- src/Easy/TwoSums.java
+++ src/Easy/TwoSums.java
@@ -5,6 +5,9 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.*/
 package Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSums {
 
 	public static void main(String[] args) {
@@ -26,20 +29,70 @@ public class TwoSums {
 			System.out.println("Failed");
 
 	}
+}
+
 	
 	
-    public static int[] twoSum(int[] nums, int target) {
-        int k[] = new int[2];
-        for (int h=0 ; h<nums.length ; h++) {
-            for (int j=h+1 ; j<nums.length ; j++) {
-                if (target == nums[h] + nums[j]) {
-                    k[0] = h;
-                    k[1] = j;
-                } 
-            }
-        }
-        return k;
-    }
+	// my approach bruth sol TC = o(n2) and sc= O(1)
+	// class Solution {
+//	     public int[] twoSum(int[] nums, int target) {
+//	         int[] result = new int[2];
+//	         for(int j = 0; j < nums.length; j++) {
+//	             for(int i = j+1; i < nums.length; i++) {
+//	                 if(nums[j] + nums[i] == target) {
+//	                     result[0] = j;
+//	                     result[1] = i;
+//	                     return result;
+//	                 }
+
+//	             }
+//	         }
+//	         return null;
+//	     }
+	// }
+
+	// not using this bcoz we need index value and not the element value, to get index value we will need to a map to store ele, index relation
+	// best approach use two pointers (left , right) TC = o(n) and sc = o(1)
+	// class Solution {
+//	     public int[] twoSum(int[] nums, int target) {
+//	         // Step 1 create two pointers
+//	         int left = 0; // iterate from left to right
+//	         int right = nums.length-1; // iterate from right to left
+
+//	         //  Step 2 sort array
+//	         Arrays.sort(nums);
+
+//	         // Step 3 iterate pointers in loop
+//	         while (left < right) {
+//	             if (nums[left] + nums[right] == target) return new int[] {nums[left], nums[right]};
+//	             else if (nums[left] + nums[right] > target) right--;
+//	             else left++;
+//	         }
+//	         // no elements whos sum is target
+//	         return null;
+//	     }
+	// }
+
+	// best approach using map  TC = O(n) and SC = O(n)
+	class SolutionTwoSums {
+	    public int[] twoSum(int[] nums, int target) {
+	        // create map to store ele and index relationship
+	        Map<Integer, Integer> m = new HashMap<>();
+	        // iterate in loop to check the target store value in map
+	        for (int i = 0 ; i < nums.length ; i++) {
+	            // check in map for target ele
+	            if (m.containsKey(target-nums[i])) {
+	                // if contain return indexs of ele's
+	                return new int[] {m.get(target-nums[i]), i};
+	            } else {
+	                //  add in map with it's index
+	                m.put(nums[i], i);
+	            }
+	        }
+	        // if no sum ele's then return null;
+	        return null;
+	    }
+	}
 	
 
 }
